39af09ec2a7136b7f6916e1f67207bab54d112eb
Lordsans-404 <70736442+Lordsans-404@users.noreply.github.com>
2025-04-23 22:03:21 +0700

    Update quotes

--- api/quotes.js
+++ api/quotes.js
@@ -1,9 +1,10 @@
 const quotes = [
-  {"id":1,"quote":"The best time for a new beginning is now.","author":"Unknown"},
-  {"id":2,"quote":"Don't put off until tomorrow what you can do today.","author":"Benjamin Franklin"},
-  {"id":3,"quote":"The journey is the destination.","author":"Confucius"},
-  {"id":4,"quote":"In the middle of difficulty lies opportunity.","author":"Albert Einstein"},
-  {"id":5,"quote":"Don't dream your life, live your dream.","author":"Unknown"}
+  {"id":1,"quotes":"The best time for a new beginning is now.","author":"Unknown"},
+  {"id":2,"quotes":"Don't put off until tomorrow what you can do today.","author":"Benjamin Franklin"},
+  {"id":3,"quotes":"The journey is the destination.","author":"Confucius"},
+  {"id":4,"quotes":"In the middle of difficulty lies opportunity.","author":"Albert Einstein"},
+  {"id":5,"quotes":"Don't dream your life, live your dream.","author":"Unknown"},
+  {"id":6,"quotes":"If you can't find a way, create a new one!","author":"Unknown"}
 ];
 
 module.exports = (req, res) => {
